4fa040f5bf260a712c4eba96e43d16895f8a8b72
BACOUL <102251510+BACOUL@users.noreply.github.com>
2026-01-06 13:05:01 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css' // Le chemin doit pointer vers le nouveau dossier styles
+import '../globals.css'
 import type { AppProps } from 'next/app'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
