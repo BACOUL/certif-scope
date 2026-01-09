8142d84b9d13ea66eca250c900c9bc365fc8957c
TonPseudoGitHub <ton@email>
2026-01-09 16:34:27 +0100

    Remove Base template wrapper from _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Base from "@/components/templates/Base";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Base>
-      <Component {...pageProps} />
-    </Base>
-  );
+  return <Component {...pageProps} />;
 }
